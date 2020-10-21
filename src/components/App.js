ed6ee2276d876d5daf4a6b2860a30411e82b9fdf
vishalvisu <vk85243@gmail.com>
2020-10-21 20:53:27 +0530

    V5

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
-var cnt = 0;
 export default function App() {
   function showNotes() {
-    if (cnt == 1) return;
     let p = document.createElement("p");
     p.id = "para";
-    p.innerText =
+    p.innerHTML =
       "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
 
     let dv = document.getElementById("main");
     dv.appendChild(p);
-    cnt++;
   }
 
   return (
