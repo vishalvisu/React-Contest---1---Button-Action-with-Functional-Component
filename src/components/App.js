ffd9d26e7ac080120f4ab57fc0a36e0377395372
vishalvisu <vk85243@gmail.com>
2020-10-21 20:33:01 +0530

    v1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [Note, update_note] = useState("");
+
+  function showNotes() {
+    update_note(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={showNotes} id="click">
+        showPara
+      </button>
+      <p id="para">{Note}</p>
     </div>
   );
 }
-
 
 export default App;
